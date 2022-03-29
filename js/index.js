f724d421e919bd985dde4bf92242e4baa09b866a
Alex Fernan <mercadoalexfernan@gmail.com>
2022-03-29 14:06:09 +0800

    Blackjack Game 1.0 done

--- js/index.js
+++ js/index.js
@@ -1,19 +1,22 @@
+let player = {
+    name: "Alex",
+    chips: 100
+}
+
 let cards = [] 
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = "" 
+
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
-player = {
-    name: "Alex",
-    chips: 100
-}
-
 let playerEl = document.getElementById("player-el")
+
 playerEl.textContent = player.name + ": $" + player.chips
+
+
 function getRandomCard() {
     let randomCardNumber = Math.floor(Math.random() * 13) + 1
         if (randomCardNumber > 10){
